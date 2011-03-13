42160228b2ec936d9658a36e7adfd51ebd748950
HardFault <laurfiac@ensea.fr>
2011-03-13 20:47:57 +0100

    Ajout de nouveaux membres à la classe Film 	modified:   src/Film.cpp 	modified:   src/Film.h

--- src/Film.cpp
+++ src/Film.cpp
@@ -5,6 +5,9 @@ Film::Film()
 	m_titre = "";
 	m_dvd = false;
 	m_fichier = "";
+	m_realisateur = "";
+	m_annee = 0;
+	m_resume = "";
 }
 
 Film::Film(string p_titre, bool p_dvd, string p_fichier)
@@ -12,6 +15,19 @@ Film::Film(string p_titre, bool p_dvd, string p_fichier)
 	m_titre = p_titre;
 	m_dvd = p_dvd;
 	m_fichier = p_fichier;
+	m_realisateur = "";
+	m_annee = 0;
+	m_resume = "";
+}
+
+Film::Film(string p_titre, bool p_dvd, string p_fichier, string p_realisateur, int p_annee, int p_resume)
+{
+	m_titre = p_titre;
+	m_dvd = p_dvd;
+	m_fichier = p_fichier;
+	m_realisateur = p_realisateur;
+	m_annee = p_annee;
+	m_resume = p_realisateur;
 }
 
 void Film::setTitre(string p_titre)
@@ -42,6 +58,36 @@ void Film::setFichier(string p_fichier)
 string Film::fichier()
 {
 	return m_fichier;
+}
+
+void Film::setRealisateur(string p_realisateur)
+{
+	m_realisateur = p_realisateur;
+}
+
+string Film::realisateur()
+{
+	return m_realisateur;
+}
+	
+void Film::setAnnee(int p_annee)
+{
+	m_annee = p_annee;
+}
+
+int Film::annee()
+{
+	return m_annee;
+}
+	
+void Film::setResume(string p_resume)
+{
+	m_resume = p_resume;
+}
+
+string Film::resume()
+{
+	return m_resume;
 }
 
 bool Film::operator<(const Film &p_film) const
--- src/Film.h
+++ src/Film.h
@@ -12,6 +12,7 @@ class Film
 public:
 	Film();
 	Film(string p_titre, bool p_dvd, string p_fichier = "");
+	Film(string p_titre, bool p_dvd, string p_fichier, string p_realisateur, int p_annee, int p_resume);
 	
 	void setTitre(string p_titre);
 	string titre();
@@ -21,6 +22,15 @@ public:
 	
 	void setFichier(string p_fichier);
 	string fichier();
+	
+	void setRealisateur(string p_realisateur);
+	string realisateur();
+	
+	void setAnnee(int p_annee);
+	int annee();
+	
+	void setResume(string p_resume);
+	string resume();
 
     bool operator<(const Film &p_film) const;
 	
@@ -29,6 +39,9 @@ private:
 	string m_titre;
 	bool m_dvd;
 	string m_fichier;
+	string m_realisateur;
+	int m_annee;
+	string m_resume;
 	
 };
 
